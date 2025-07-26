98f8396c5865204d96a313f3a86d9889fd6bb62e
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-07-26 10:36:24 +0530

    donw with overall website looking for other changes to do in future if neddded and learned a lot from this project faced many problems but i did  not give up 📍

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,28 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  { href: "https://github.com/Zuhaib-dev/", icon: <FaGithub /> },
+  { href: "https://www.linkedin.com/in/xuhaib-rashid-661345318", icon: <FaLinkedin /> },
+  { href: "https://www.instagram.com/xuhaib_x9", icon: <FaInstagram /> },
+  { href: "https://x.com/xuhaib_x9", icon: <FaTwitter /> },
 ];
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          ©Zuhaib {year}. All rights reserved
         </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
+        <div className="flex justify-center gap-4 md:justify-start">
           {socialLinks.map((link, index) => (
             <a
               key={index}
